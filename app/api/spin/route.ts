19867c8e466203384782bfb8e8cae68255c82eba
Nhat Anh Dang <awmtrg21@gmail.com>
2024-12-06 16:15:38 +0700

    semi final stage

--- app/api/spin/route.ts
+++ app/api/spin/route.ts
@@ -1,25 +1,19 @@
 import { NextResponse } from "next/server";
-// import fs from "fs";
-// import path from "path";
+import fs from "fs";
+import path from "path";
 
-// const dataFilePath = path.join(process.cwd(), "data", "pies.json");
+const dataFilePath = path.join(process.cwd(), "data", "pies.json");
 
 export async function POST() {
   try {
-    const prizes = [
-      "Gift Voucher 5%",
-      "Gift Voucher 10%",
-      "100,000 VND",
-      "Better Luck Next Time",
-      "Gift Voucher 20%",
-      "Gift Voucher 50%",
-    ];
+    const jsonData = fs.readFileSync(dataFilePath, "utf-8");
+    const prizes = JSON.parse(jsonData);
 
     // Randomly pick a prize
     const randomIndex = Math.floor(Math.random() * prizes.length);
     const selectedPrize = prizes[randomIndex];
 
-    return NextResponse.json({ prize: selectedPrize, index: randomIndex });
+    return NextResponse.json({ selectedPrize });
   } catch (error) {
     console.log(error);
     return NextResponse.json(
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,43 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(true);
+  const [img, setImg] = useState("");
+  const [message, setMessage] = useState("");
+  const [title, setTitle] = useState("");
+
   let currentRotation = 0;
   const spinSpeed = 10;
 
-  const spin = () => {
-    const wheel = document.getElementById("wheel");
+  const spin = async () => {
+    const wheels = document.querySelectorAll(".wheel");
     const spinningInterval = setInterval(() => {
       currentRotation = (currentRotation + spinSpeed) % 360;
-      wheel!.style.transform = `rotate(${currentRotation}deg)`;
+      for (const element of wheels) {
+        const wheel = element as HTMLElement;
+        wheel.style.transform = `rotate(${currentRotation}deg)`;
+      }
     }, 20);
 
-    setTimeout(() => {
-      clearInterval(spinningInterval);
-    }, 10000);
+    const response = await fetch("/api/spin", { method: "POST" });
+    const result = await response.json();
+
+    clearInterval(spinningInterval);
+    currentRotation = 0;
+    for (const element of wheels) {
+      const wheel = element as HTMLElement;
+      wheel.style.transform = `rotate(${currentRotation}deg)`;
+    }
+
+    if (result && result.selectedPrize) {
+      setShowModal(true);
+      setImg(result.selectedPrize.img);
+      setMessage(result.selectedPrize.message);
+      setTitle(result.selectedPrize.title);
+    }
   };
 
   return (
@@ -44,8 +66,7 @@ export default function Home() {
         <div className="main md:flex md:h-[calc(100vh-130px)] items-center justify-center md:overflow-hidden md:px-[100px]">
           <div className="hidden md:block absolute left-1/4 top-1/2 -translate-y-1/2 -translate-x-1/2">
             <Image
-              id="wheel"
-              className="aspect-square !max-w-[600px]"
+              className="wheel aspect-square !max-w-[600px]"
               aria-hidden
               src="/wheel.png"
               alt="Nagakawa"
@@ -60,8 +81,7 @@ export default function Home() {
               Chào mừng đến với vòng quay may mắn
             </h3>
             <Image
-              id="wheel"
-              className="md:hidden aspect-square"
+              className="wheel md:hidden aspect-square"
               aria-hidden
               src="/wheel.png"
               alt="Nagakawa"
@@ -110,6 +130,76 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-center justify-center p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3
+                    className="text-3xl text-[#046B38] text-center font-semibold"
+                    dangerouslySetInnerHTML={{ __html: title }}
+                  ></h3>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <div className="flex justify-center items-center">
+                    {img ? (
+                      <Image
+                        className=""
+                        aria-hidden
+                        src={img}
+                        alt="Nagakawa"
+                        width={100}
+                        height={0}
+                        priority
+                      />
+                    ) : null}
+                  </div>
+                  <p className="my-4 text-black text-center text-lg leading-relaxed">
+                    {message ? (
+                      message
+                    ) : (
+                      <>
+                        Tiếc quá, bạn đã hết lượt chơi mất rồi.
+                        <br />
+                        Hẹn gặp lại bạn lần sau
+                      </>
+                    )}
+                    {message && title ? (
+                      <>
+                        <hr />
+                        <p>
+                          Vui lòng liên hệ hotline{" "}
+                          <a href="callto:1900.545.489" title="1900.545.489">
+                            1900.545.489
+                          </a>
+                          <br />
+                          hoặc FB/Zalo Nakagawa để nhận thưởng
+                        </p>
+                      </>
+                    ) : null}
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Đóng
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </>
   );
 }
