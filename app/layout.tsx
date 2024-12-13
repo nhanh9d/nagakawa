2701fd5214c94bd44aa9903f26ff479f21ba91fa
An <awmtrg21@gmail.com>
2024-12-13 21:07:04 +0700

    test speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 
 import { Inter, Lobster } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased ${lobster.className} ${inter.className}`}
       >
         {children}
+        <SpeedInsights/>
       </body>
     </html>
   );
